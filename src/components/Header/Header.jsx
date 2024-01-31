8d0fff6bd32ab0f9a907c2e458439b93adee5e7a
Kaique Costa <kaiquenagato94@outlook.com>
2024-01-31 16:40:11 -0300

    extend tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import ToogleTheme from "../ToogleTheme/TootgleTheme"
 
 const Header = () => {
-  return (
-  <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:m-5">
+  return <div className="flex h-20 bg-alura-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">Olá usuário</span>
       <h1 className="text-gray-100 text-xl">Kaique NewsLetter</h1>
       <ToogleTheme className="h-8 hidden text-gray-100 sm:block cursor-pointer"/>
     </div>
-  )
+  
 }
 
 export default Header
