ab21ff08cf6044e21724c66109535e947e03897f
Kaique Costa <kaiquenagato94@outlook.com>
2024-02-05 10:20:19 -0300

    estilizando listas

--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -1,10 +1,11 @@
 import data from '../../../articles.json'
+import Article from '../Article/Article'
 
 const ArticlesList = () => {
 
   return <div className='mt-5 grid gap-5 sm:mt-0 sm:grid-cols-2 lg:grid-cols-3 m-auto max-w-2xl'>
     {         // eslint-disable-next-line react/prop-types
-            data.map((props, index) => <p key={index}>{props.title}</p>)
+            data.map((props, index) => <Article key={index}{...props}/>)
         }
   </div>
 }
